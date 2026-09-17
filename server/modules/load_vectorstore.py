e3829a211cc1b110556ca3179e73874b7aa0ed84
Manoj Kumar <manojkhatri.me@gmail.com>
2026-09-17 18:53:53 +0500

    git commit -m "Fix Gemini embedding model"

--- server/modules/load_vectorstore.py
+++ server/modules/load_vectorstore.py
@@ -43,7 +43,10 @@ index=pc.Index(PINECONE_INDEX_NAME)
 # load,split,embed and upsert pdf docs content
 
 def load_vectorstore(uploaded_files):
-    embed_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    embed_model = GoogleGenerativeAIEmbeddings(
+    model="gemini-embedding-001",
+    output_dimensionality=768
+)
     file_paths = []
 
     for file in uploaded_files:
